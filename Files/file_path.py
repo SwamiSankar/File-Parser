31c17c647d5a721f5b9a1cd8ebc5401bc08c627a
SwamiSankar <swamisankar2008@gmail.com>
2021-07-19 11:39:49 +0530

    Command line corrections

--- Files/file_path.py
+++ Files/file_path.py
@@ -1,4 +1,6 @@
 import os
 current_working_directory = os.getcwd()
-os.environ['FILE_PATH'] = '{}/Files/*csv'.format(current_working_directory)
-os.environ['FILE_PATH_REDUCED'] = "{}/Files/".format(current_working_directory)
+os.environ['FILE_PATH'] = '{}/File-Parser/Files/*csv'.format(
+    current_working_directory)
+os.environ['FILE_PATH_REDUCED'] = "{}/File-Parser/Files/".format(
+    current_working_directory)
